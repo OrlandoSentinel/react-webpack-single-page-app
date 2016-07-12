730e55e699a1df42c52b4dd20a7238bc3b6d0918
Gibson, Andrew <agibson@orlandosentinel.com>
2016-07-12 14:38:34 -0400

    make template OS-friendly

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,38 +6,12 @@ import styles from './app.scss';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    
-    this.state = {
-        mounted: false
-    };
-  }
-  
-  componentDidMount() {
-    let self = this;
-    
-    setInterval(function(){
-        self.setState({
-            mounted: true
-        });
-    }, 2000);
   }
 
   render() {
-    let class_name = '';
-    
-    if (!this.state.mounted) {
-        class_name = styles.unmounted;
-    } else {
-        class_name = styles.mounted;
-    }
-  
     return (
-      <div>
-        <p className={class_name}>Hello world. My color change is controlled by CSS modules, and my media query size change is controlled by a Sass mixin.</p>
-        <img src={require('../../../assets/images/placeholder.png')} />
-        <p>I am an image that is required in a React component.</p>
-        <p className={styles.font_example}>My font is loaded by webfontloader.</p>
-        <div className={styles.clearfix}>I am a floated div that uses a clearfix mixin.</div>
+      <div className={styles.app}>
+        <p>Hi, React just appended me to the page.</p>
       </div>
     );
   }
